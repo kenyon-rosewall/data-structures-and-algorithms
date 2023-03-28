d02f4ec9656d6935eaa7b4028bbf1ea73a7fe956
Kenyon Rosewall <pgurugp@gmail.com>
2023-03-28 18:53:23 -0500

    Added documentation

--- stack/stack.h
+++ stack/stack.h
@@ -8,12 +8,25 @@
 // 
 // Stacks
 //
+// I decided to implement this with an array for two reasons. One, I have been using linked
+// lists for other things such as graphs. Two, seeing as how a stacks I'm familiar with are
+// linked to the hardware and have a hard limit and generate the condition of the now topical 
+// phrase "stack overflow." I also don't clear out the values of the popped items, which will
+// lead to garbage data if you were to just look at the stack in its capacity. I'm fine with
+// this solution.
 //
+// Due to Cs limitation at returning NULL for int types, I decided to return INT_MIN from 
+// stack_pop() when there wasn't any items in the stack. I could have gone a different way
+// with this, but I'm okay with this. It couldn't be something simple like -1 because I am
+// allowing the items to be negatve numbers all well. I also could have done somethnig similar
+// when the stack was full, but I currently return NULL and print out to the stdout "Stack
+// Overflow", even though I am aware that this isn't the error here, should just be something 
+// like "The stack is full."
 //
 
 //
 // TODO
-// * 
+// * Implementing stack to perform tasks (infix expressions, reversing strings)
 //
 
 typedef struct stack stack;
