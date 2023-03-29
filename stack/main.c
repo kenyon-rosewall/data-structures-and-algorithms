957b9cbe01eec9927eda78ebf71631bfcfd25992
Kenyon Rosewall <pgurugp@gmail.com>
2023-03-28 22:17:59 -0500

    Adding more tests

--- stack/main.c
+++ stack/main.c
@@ -21,12 +21,17 @@ i32 main(i32 argc, char **argv)
 {
   b32 dot = is_dot(argc, argv);
 
-  stack* s = stack_create(25);
+  stack* s = stack_create(6);
+  i32 popped_empty = stack_pop(s);
+  stack_push(s, 50);
   stack_push(s, 10);
   stack_push(s, 20);
   stack_push(s, 30);
   i32 popped = stack_pop(s);
-  stack_push(s, 50);
+  stack_push(s, 51);
+  stack_push(s, 52);
+  stack_push(s, 53);
+  stack_push(s, 55);
 
   if (dot)
   {
@@ -35,7 +40,14 @@ i32 main(i32 argc, char **argv)
   else
   {
     stack_print(s);
+    
+    printf("Stack is empty: ");
+    stack_empty(s) ? printf("YES\n") : printf("NO\n");
+    printf("Stack is full: ");
+    stack_full(s) ? printf("YES\n") : printf("NO\n");
+
     printf("Stack top is %d\n", stack_peek(s));
+    printf("Popped while empty value is %d\n", popped_empty); 
     printf("Popped value is %d\n", popped);
   }
 
